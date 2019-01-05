d0c3a79d006d6b783a18fc8835021a65e0b25f3c
osoriano <osoriano2@gmail.com>
2019-01-05 05:20:36 +0000

    Upgrade winston logger

--- packages/waves-server-logger/index.js
+++ packages/waves-server-logger/index.js
@@ -1,22 +1,14 @@
-const log = require('winston');
+const winston = require('winston');
 
-log.remove(log.transports.Console);
+const log = winston.createLogger({
+  level: 'debug',
+  format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.simple()
+  ),
+  transports: [
+    new winston.transports.Console()
+  ]
+});
 
-log.add(log.transports.Console, {
-    formatter: function(options) {
-      var rawMsg = (options.message !== undefined ? options.message : '');
-      var rawMeta
-      try {
-        rawMeta = (options.meta && Object.keys(options.meta).length ?
-                   '\n' + JSON.stringify(options.meta, null, 3) : '' );
-      } catch (e) {
-        rawMeta = options.meta
-      }
-      var msg = '[' + options.level + '] ' + rawMsg + rawMeta;
-      return log.config.colorize(options.level, msg);
-    }
-  }
-);
-
-log.level = 'debug'
 module.exports = log
