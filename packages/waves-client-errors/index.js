b827648f160d29b76fefb27a1ddfee76a0bea00c
osoriano <osoriano2@gmail.com>
2019-01-26 23:56:03 +0000

    Add custom TrackUpload error

---
+++ packages/waves-client-errors/index.js
@@ -0,0 +1,17 @@
+const MESSAGE = 'Failed to upload track'
+
+class UploadError extends Error {
+  constructor(track, cause) {
+    super(MESSAGE);
+    this.message = MESSAGE;
+    this.name = this.constructor.name
+    this.track = track
+    this.cause = cause
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, UploadError);
+    }
+  }
+}
+
+module.exports.UploadError = UploadError
