58e503fb3fa4ef60bdb33a6deba706b35b046d24
osoriano <osoriano2@gmail.com>
2019-02-05 02:42:31 +0000

    ws fix

--- packages/waves-client-web/src/components/table/dragbox.js
+++ packages/waves-client-web/src/components/table/dragbox.js
@@ -5,22 +5,22 @@ export default function getDragCanvas(numSelected) {
   canvas.width = DRAG_BOX_WIDTH
   canvas.height = DRAG_BOX_HEIGHT
 
-	ctx.fillStyle = 'DRAG_BOX_BORDER_COLOR'
-	fillRoundRect(ctx, 0, 0, DRAG_BOX_WIDTH, DRAG_BOX_HEIGHT, DRAG_BOX_BORDER_RADIUS)
+  ctx.fillStyle = 'DRAG_BOX_BORDER_COLOR'
+  fillRoundRect(ctx, 0, 0, DRAG_BOX_WIDTH, DRAG_BOX_HEIGHT, DRAG_BOX_BORDER_RADIUS)
 
-	ctx.fillStyle = 'DRAG_BOX_BACKGROUND_COLOR'
-	ctx.fillRect(DRAG_BOX_BORDER_WIDTH, DRAG_BOX_BORDER_WIDTH,
+  ctx.fillStyle = 'DRAG_BOX_BACKGROUND_COLOR'
+  ctx.fillRect(DRAG_BOX_BORDER_WIDTH, DRAG_BOX_BORDER_WIDTH,
                DRAG_BOX_WIDTH - 2 * DRAG_BOX_BORDER_WIDTH,
                DRAG_BOX_HEIGHT - 2 * DRAG_BOX_BORDER_WIDTH)
 
-	ctx.fillStyle = 'white'
-	ctx.textBaseline = 'middle'
+  ctx.fillStyle = 'white'
+  ctx.textBaseline = 'middle'
 
-	ctx.font = '17px/1.42857143 "Open Sans", sans-serif'
-	ctx.fillText('♫', 22, 25)
+  ctx.font = '17px/1.42857143 "Open Sans", sans-serif'
+  ctx.fillText('♫', 22, 25)
 
-	ctx.font = '14px/1.42857143 "Open Sans", sans-serif'
-	ctx.fillText(`Add ${numSelected} tracks`, 60, 25)
+  ctx.font = '14px/1.42857143 "Open Sans", sans-serif'
+  ctx.fillText(`Add ${numSelected} tracks`, 60, 25)
   return canvas
 }
 
@@ -28,17 +28,17 @@ export default function getDragCanvas(numSelected) {
 // Based on: https://stackoverflow.com/questions/1255512/
 // how-to-draw-a-rounded-rectangle-on-html-canvas
 function fillRoundRect(ctx, x, y, width, height, radius) {
-	ctx.beginPath()
-	ctx.moveTo(x + radius, y)
-	ctx.lineTo(x + width - radius, y)
-	ctx.quadraticCurveTo(x + width, y, x + width, y + radius)
-	ctx.lineTo(x + width, y + height - radius)
-	ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height)
-	ctx.lineTo(x + radius, y + height)
-	ctx.quadraticCurveTo(x, y + height, x, y + height - radius)
-	ctx.lineTo(x, y + radius)
-	ctx.quadraticCurveTo(x, y, x + radius, y)
-	ctx.closePath()
+  ctx.beginPath()
+  ctx.moveTo(x + radius, y)
+  ctx.lineTo(x + width - radius, y)
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius)
+  ctx.lineTo(x + width, y + height - radius)
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height)
+  ctx.lineTo(x + radius, y + height)
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius)
+  ctx.lineTo(x, y + radius)
+  ctx.quadraticCurveTo(x, y, x + radius, y)
+  ctx.closePath()
 
-	ctx.fill()
+  ctx.fill()
 }
