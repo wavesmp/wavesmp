70851ed8e9cc85bb6a82577180a0da738cd521aa
osoriano <osoriano2@gmail.com>
2019-03-05 05:52:13 +0000

    Cleanup columns

--- packages/waves-client-web/src/components/table/columns.js
+++ packages/waves-client-web/src/components/table/columns.js
@@ -4,7 +4,6 @@ import { Name, State, Time, Artist, Album, Genre, CreatedAt } from './rows'
 
 /*  TODO integrate common-columns-small-screen-hide
  *       with displayed columns in localstorage */
-/* TODO refer to attribute in TrackInfoUpdate or refactor into constants */
 export const playlistColumns = [
   {
     title: 'Name',
@@ -13,7 +12,6 @@ export const playlistColumns = [
   },
   {
     title: 'State',
-    attribute: 'FIXME',
     Component: State
   },
   {
@@ -42,19 +40,5 @@ export const playlistColumns = [
     Component: CreatedAt
   }
 ]
-export const libraryColumns = clonePlaylistColumns()
-libraryColumns.forEach(column => {
-  // TODO code smell? may be better to duplicate list
-  if (column.attribute !== 'FIXME') {
-    column.sortable = true
-  }
-})
 
-// TODO consider using jquery func
-function clonePlaylistColumns() {
-  const clonedColumns = []
-  for (let i = 0; i < playlistColumns.length; i += 1) {
-    clonedColumns.push({ ...playlistColumns[i] })
-  }
-  return clonedColumns
-}
+export const libraryColumns = playlistColumns.map(col => ({...col, sortable: !!col.attribute}))
