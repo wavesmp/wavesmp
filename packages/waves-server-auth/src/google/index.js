271b88c12afaa9264d2dc142ab0bd3654aab7007
osoriano <osoriano2@gmail.com>
2019-05-19 20:39:50 +0000

    Use google auth lib for token verify

--- packages/waves-server-auth/src/google/index.js
+++ packages/waves-server-auth/src/google/index.js
@@ -1,38 +1,19 @@
-const rp = require('request-promise')
-
-const log = require('waves-server-logger')
-
-const LOGIN_URL = 'https://www.googleapis.com/oauth2/v3/tokeninfo?id_token='
-const IDP = 'google'
+const { OAuth2Client } = require('google-auth-library')
 
 class Auth {
   constructor({ clientIds }) {
     this.clientIds = clientIds
+    this.client = new OAuth2Client()
   }
 
   async login(token) {
-    let authResp
-    try {
-      authResp = await rp({
-        uri: LOGIN_URL + token,
-        json: true // Parse the JSON string in the response
-      })
-    } catch (err) {
-      const errMsg = `Unable to authenticate user: ${err}`
-      log.error(errMsg)
-      throw new Error(errMsg)
-    }
-
-    // sub - idp id
-    // aud - client id
-    const { aud, email, name, sub } = authResp
-    if (this.clientIds.indexOf(aud) < 0) {
-      const errMsg = `Invalid client id ${aud} found for user ${name} (${email})`
-      log.error(errMsg)
-      throw new Error(errMsg)
-    }
-
-    return { idp: IDP, idpId: sub, name, email }
+    const ticket = await this.client.verifyIdToken({
+      idToken: token,
+      audience: this.clientIds
+    })
+    const payload = ticket.getPayload()
+    const { email, name, sub } = payload
+    return { idp: 'google', idpId: sub, name, email }
   }
 }
 
