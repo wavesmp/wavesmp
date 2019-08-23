4ff0b00ff59476c6470a8fd7504a40a1dba42aa5
osoriano <osoriano2@gmail.com>
2019-08-23 03:49:35 +0000

    Add actions.contextmenuSetElem
    This allows setting the contextmenu based on
    the position of the element

--- packages/waves-client-actions/src/contextmenu.js
+++ packages/waves-client-actions/src/contextmenu.js
@@ -4,8 +4,7 @@ function contextmenuReset() {
   return { type: types.CONTEXTMENU_RESET }
 }
 
-function getTransform(ev) {
-  const { pageX: x, pageY: y } = ev
+function getTransform(x, y) {
   const { pageXOffset, innerWidth } = window
   const hasRightSpace = pageXOffset + innerWidth - x >= CONTEXTMENU_WIDTH
   if (hasRightSpace) {
@@ -18,7 +17,29 @@ function getTransform(ev) {
  * i.e. ev.pageX, ev.pageY */
 function contextmenuSet(menu) {
   const { ev } = menu
-  menu.transform = getTransform(ev)
+  const { pageX: x, pageY: y } = ev
+  menu.transform = getTransform(x, y)
+  return { type: types.CONTEXTMENU_SET, menu }
+}
+
+function getOffset(elem) {
+  const { left, top } = elem.getBoundingClientRect()
+  return {
+    left: left + window.scrollX,
+    top: top + window.scrollY
+  }
+}
+
+/* Set context menu based on element position
+ * i.e. ev.currentTarget */
+function contextmenuSetElem(menu) {
+  const { ev } = menu
+  const { currentTarget: elem } = ev
+  const { offsetWidth: width, offsetHeight: height } = elem
+  const { left, top } = getOffset(elem)
+  const x = left + width
+  const y = top + height
+  menu.transform = getTransform(x, y)
   return { type: types.CONTEXTMENU_SET, menu }
 }
 
@@ -32,5 +53,6 @@ function contextmenuBack() {
 
 module.exports.contextmenuReset = contextmenuReset
 module.exports.contextmenuSet = contextmenuSet
+module.exports.contextmenuSetElem = contextmenuSetElem
 module.exports.contextmenuNext = contextmenuNext
 module.exports.contextmenuBack = contextmenuBack
